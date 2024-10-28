fad4e70e32fbafd67d95cd718185619282edf143
TiaUAT101 <tmitchell77212@gmail.com>
2024-10-27 18:48:15 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,34 @@
+// Function to simulate rolling a dice when the button is clicked
+function rollDice() {
+    // Generate a random number between 1 and 6
+    const diceRoll = Math.floor(Math.random() * 6) + 1;
+
+    // Output message variable
+    let message;
+
+    // Conditional statements to determine the outcome based on the dice roll
+    if (diceRoll === 1) {
+        message = "Oh no! You rolled a 1. Better luck next time!";
+    } else if (diceRoll <= 3) {
+        message = "You rolled a " + diceRoll + ". Not bad!";
+    } else if (diceRoll <= 5) {
+        message = "Great! You rolled a " + diceRoll + ". Keep it up!";
+    } else {
+        message = "Jackpot! You rolled a 6! You win!";
+    }
+
+    // Output the result to the webpage using innerHTML
+    document.getElementById('gameOutput').innerHTML = message;
+}
+
+// Function to set up the event listener for the button
+function setupGame() {
+    // Get the button element by its ID
+    const button = document.getElementById('rollButton');
+
+    // Add click event listener to the button
+    button.addEventListener('click', rollDice);
+}
+
+// Call setupGame when the script loads
+setupGame();
